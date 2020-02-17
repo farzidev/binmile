252bb0dbd91f9477be565f42b7ed7aad919a955e
Pranav Singh Gour <pranavg456@gmail.com>
2020-02-17 19:28:19 +0530

    add sample post detail

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import IndexView, BlogView, ServicesView
+from .views import IndexView, BlogView, ServicesView, PostSampleView, PostDetailView
 
 app_name = 'posts'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('blog/', BlogView.as_view(), name='blog'),
     path('services', ServicesView.as_view(), name='services'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/', PostSampleView.as_view(), name='post-sample'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 # from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+
+from .models import Post
 
 
 class IndexView(TemplateView):
@@ -12,3 +15,13 @@ class BlogView(TemplateView):
 
 class ServicesView(TemplateView):
     template_name = "service.html"
+
+
+class PostSampleView(TemplateView):
+    template_name = "binmile_article.html"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    queryset = Post.objects.all()
+    template_name = "binmile_article.html"
