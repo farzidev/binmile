2d6e495d162f7d58ac91c89f932f726d01518342
Pranav Singh Gour <pranavg456@gmail.com>
2020-02-27 01:49:24 +0530

    improve Post admin

--- posts/admin.py
+++ posts/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Post, Category, Form
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ("title", "category")
+    list_filter = ("category", "timestamp")
+    search_fields = ("title", "content")
+
+
 admin.site.register(Category)
 admin.site.register(Form)
