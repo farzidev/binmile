a9bb86491c736175898b2ee25933dc77a8d91dc8
Pranav Singh Gour <pranavg456@gmail.com>
2020-02-27 17:48:19 +0530

    seperate prod & dev settings

--- binmile/settings/base.py
+++ binmile/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,25 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'binmile.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'binmile',
-        'USER': 'master',
-        'PASSWORD': '726f01569f5d4b8dbfa7',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
 
 
 # Password validation
---
+++ binmile/settings/production.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'binmile',
+        'USER': 'master',
+        'PASSWORD': '726f01569f5d4b8dbfa7',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
