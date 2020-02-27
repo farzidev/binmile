2d0c127feac2feadc55b44ea5439083c15c48088
Pranav Singh Gour <pranavg456@gmail.com>
2020-02-27 17:23:00 +0530

    config postgres database

--- binmile/settings.py
+++ binmile/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'binmile.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'binmile',
+        'USER': 'master',
+        'PASSWORD': '726f01569f5d4b8dbfa7',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
