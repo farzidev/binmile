68214d0f699ed90f9c779e0241b4deed227a2438
Pranav Singh Gour <pranavg456@gmail.com>
2020-02-29 00:12:28 +0530

    contact form working

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
 # from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import DetailView
+from django.views.generic import DetailView, FormView
+from django.contrib import messages
 
-from .models import Post, Category
+from .models import Post, Category, Form
+from .forms import ContactForm
 
 
 class IndexView(TemplateView):
@@ -40,9 +42,37 @@ class AboutUsView(TemplateView):
     template_name = "aboutUs.html"
 
 
-class ContactUsView(TemplateView):
+class ContactUsView(FormView):
     template_name = "contact.html"
+    form_class = ContactForm
+    success_url = "#contactForm"
+    page = "contact"
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super().get_form_kwargs(**kwargs)
+        kwargs.update({'page': self.page})
+        return kwargs
+
+    def form_valid(self, form):
+        Form.objects.get_or_create(**form.cleaned_data)
+        messages.success(self.request, "Thanks for submitting the form.")
+        form.send_mail()
+        return super().form_valid(form)
 
 
-class MicrosoftCrmView(TemplateView):
+class MicrosoftCrmView(FormView):
     template_name = "microsoftCRM.html"
+    form_class = ContactForm
+    success_url = "#consultForm"
+    page = "microsoft"
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super().get_form_kwargs(**kwargs)
+        kwargs.update({'page': self.page})
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Thanks for submitting the form.")
+        form.send_mail()
+        return super().form_valid(form)
