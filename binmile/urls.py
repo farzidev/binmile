9dadf8f667de33a16aae499cea6dc1235b563972
Pranav SG <pranavg456@gmail.com>
2020-03-30 23:04:04 +0530

    redirect to home on 404

--- binmile/urls.py
+++ binmile/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
 
 # if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# custom 404 handler
+handler404 = 'posts.views.custom_404_handler'
