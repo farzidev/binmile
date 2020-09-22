4ab7cb9aaf62440f96cd13bd7d4a770a7af5ef34
Khushi <khushisrivastava53@gmail.com>
2020-09-23 00:51:08 +0530

    DB configuration restored

--- binmile/settings/dev.py
+++ binmile/settings/dev.py
@@ -1,17 +1,17 @@
-# import os
+import os
 
-# from .base import *
+from .base import *
 
 
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-# # Database
-# # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
