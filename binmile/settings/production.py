73355df77e2ce0f4ea8cd22f0e6877ff847c2917
Khushi <khushisrivastava53@gmail.com>
2020-09-22 23:53:06 +0530

    Delay in popup

--- binmile/settings/production.py
+++ binmile/settings/production.py
@@ -1,17 +1,17 @@
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'binmile',
-        'USER': 'master',
-        'PASSWORD': '726f01569f5d4b8dbfa7',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'binmile',
+#         'USER': 'master',
+#         'PASSWORD': '726f01569f5d4b8dbfa7',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
